6ccec9cb9a33e27cab5afbd73d624b69594ec39a
ankit joshi <ankitjoshicr7@gmail.com>
2020-04-11 11:17:46 +0530

    Fixed Bugs

--- index.js
+++ index.js
@@ -72,6 +72,5 @@ app.get("/",function(req,res){
 //----------------------Assign Port number for local host---------------------------------
 
 app.listen(process.env.PORT,process.env.IP,function(){
-    console.log("Server has started");
-}
-//----------------------------------------------------------------------------------
+    console.log("Server has started")
+});
