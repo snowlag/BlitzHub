fdeb341624b943f39ea22f2f066281d394a52d2d
ankit joshi <ankitjoshicr7@gmail.com>
2020-04-11 11:19:42 +0530

    Fixed Bugs

--- models/User.js
+++ models/User.js
@@ -5,14 +5,8 @@ var userSchema = new mongoose.Schema({
     username: String,
     password: String,
     created: {type: Date , default: Date.now },
-<<<<<<< HEAD
-    birthdate: Date
-
-=======
     birthdate: Date,
     isAdmin: {type : Boolean , default : false}
->>>>>>> localbranch
-    
 })
 
 userSchema.plugin(passportLocalMongoose);
